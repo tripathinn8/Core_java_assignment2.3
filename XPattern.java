7528d6918b2ad30e417ea97ad4e4ec1f93658615
tripathinn8 <30956659+tripathinn8@users.noreply.github.com>
2017-08-19 18:27:25 +0530

    Add files via upload

---
+++ XPattern.java
@@ -0,0 +1,25 @@
+
+public class XPattern 
+{
+
+	public static void main(String[] args)
+	{
+		
+int i, j, k;
+
+for (i=1;i<=5;i++)
+{
+
+		for (j=1;j<=5;j++)
+	{
+		
+		if( (i==j) || (i+j)==6)
+			{System.out.print("*");}
+	
+		else
+			{System.out.print(" ");}
+	}
+	System.out.println();
+}
+}
+	}
